944f5a5804fc83fb18a50d03a7d848cfa8d7e045
Noemi Millman <noemi@tid.al>
2014-10-21 19:14:12 +0000

    prelim adaptation; committing so as not to lose work as we play with libphutil

--- src/Tidal/ArcArtLinter/ArcanistArtisanLinter.php
+++ src/Tidal/ArcArtLinter/ArcanistArtisanLinter.php
@@ -6,27 +6,35 @@
  */
 class ArcanistArtisanLinter extends ArcanistExternalLinter {
 	private $reports;
+	private $phpcs_linter;
+
+	public function __construct() {
+		parent::__construct();
+
+		$self->phpcs_linter = New ArcanistPhpcsLinter();
+	}
 
 	public function getInfoName() {
-		return 'PHP_CodeSniffer';
+		return 'PHP_CodeSniffer_via_Artisan';
 	}
 
 	public function getInfoURI() {
-		return 'http://pear.php.net/package/PHP_CodeSniffer/';
+		return 'https://github.com/brodkinca/BCA-Laravel-Inspect';
 	}
 
 	public function getInfoDescription() {
 		return pht(
 			'PHP_CodeSniffer tokenizes PHP, JavaScript and CSS files and '.
-			'detects violations of a defined set of coding standards.');
+			'detects violations of a defined set of coding standards. '.
+			'This variant wraps a Laravel Artisan integration of PHPCS.');
 	}
 
 	public function getLinterName() {
-		return 'PHPCS';
+		return 'PHPCS_Artisan';
 	}
 
 	public function getLinterConfigurationName() {
-		return 'phpcs';
+		return 'phpcs_artisan';
 	}
 
 	public function getMandatoryFlags() {
@@ -34,38 +42,19 @@ class ArcanistArtisanLinter extends ArcanistExternalLinter {
 	}
 
 	public function getInstallInstructions() {
-		return pht('Install PHPCS with `pear install PHP_CodeSniffer`.');
+		return 'By installing this package, you\'ve already installed all dependencies!';
 	}
 
 	public function getDefaultFlags() {
-		$options = $this->getDeprecatedConfiguration('lint.phpcs.options', array());
-		$standard = $this->getDeprecatedConfiguration('lint.phpcs.standard');
-
-		if (!empty($standard)) {
-			if (is_array($options)) {
-				$options[] = '--standard='.$standard;
-			} else {
-				$options .= ' --standard='.$standard;
-			}
-		}
-
-		return $options;
+		return $this->getDeprecatedConfiguration('lint.phpcs_artisan.options', array());
 	}
 
 	public function getDefaultBinary() {
-		return $this->getDeprecatedConfiguration('lint.phpcs.bin', 'phpcs');
+		return $this->getDeprecatedConfiguration('lint.phpcs_artisan.bin', 'artisan inspect:sniff');
 	}
 
 	public function getVersion() {
 		list($stdout) = execx('%C --version', $this->getExecutableCommand());
-
-		$matches = array();
-		$regex = '/^PHP_CodeSniffer version (?P<version>\d+\.\d+\.\d+)\b/';
-		if (preg_match($regex, $stdout, $matches)) {
-			return $matches['version'];
-		} else {
-			return false;
-		}
 	}
 
 	public function shouldExpectCommandErrors() {
@@ -77,63 +66,14 @@ class ArcanistArtisanLinter extends ArcanistExternalLinter {
 	}
 
 	protected function parseLinterOutput($path, $err, $stdout, $stderr) {
-		// NOTE: Some version of PHPCS after 1.4.6 stopped printing a valid, empty
-		// XML document to stdout in the case of no errors. If PHPCS exits with
-		// error 0, just ignore output.
-		if (!$err) {
-			return array();
-		}
-
-		$report_dom = new DOMDocument();
-		$ok = @$report_dom->loadXML($stdout);
-		if (!$ok) {
-			return false;
-		}
-
-		$files = $report_dom->getElementsByTagName('file');
-		$messages = array();
-		foreach ($files as $file) {
-			foreach ($file->childNodes as $child) {
-				if (!($child instanceof DOMElement)) {
-					continue;
-				}
-
-				if ($child->tagName == 'error') {
-					$prefix = 'E';
-				} else {
-					$prefix = 'W';
-				}
-
-				$code = 'PHPCS.'.$prefix.'.'.$child->getAttribute('source');
-
-				$message = new ArcanistLintMessage();
-				$message->setPath($path);
-				$message->setLine($child->getAttribute('line'));
-				$message->setChar($child->getAttribute('column'));
-				$message->setCode($code);
-				$message->setDescription($child->nodeValue);
-				$message->setSeverity($this->getLintMessageSeverity($code));
-
-				$messages[] = $message;
-			}
-		}
-
-		return $messages;
+		return $self->phpcs_linter->parseLinterOutput($path, $err, $stdout, $stderr);
 	}
 
 	protected function getDefaultMessageSeverity($code) {
-		if (preg_match('/^PHPCS\\.W\\./', $code)) {
-			return ArcanistLintSeverity::SEVERITY_WARNING;
-		} else {
-			return ArcanistLintSeverity::SEVERITY_ERROR;
-		}
+		return $self->phpcs_linter->getDefaultMessageSeverity($code);
 	}
 
 	protected function getLintCodeFromLinterConfigurationKey($code) {
-		if (!preg_match('/^PHPCS\\.(E|W)\\./', $code)) {
-			throw new Exception(
-				"Invalid severity code '{$code}', should begin with 'PHPCS.'.");
-		}
-		return $code;
+		return $self->phpcs_linter->getLintCodeFromLinterConfigurationKey($code);
 	}
 }
